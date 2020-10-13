288ad250f0941623a5819e68d23f08f8968e19ee
Esamaeilbeygi <m.esmaeilbeygi@gmail.com>
2020-10-13 21:53:53 +0330

    Update token letterBytes

--- pkg/utils/random.go
+++ pkg/utils/random.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_"
 const (
 	letterIdxBits = 6                    // 6 bits to represent a letter index
 	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
